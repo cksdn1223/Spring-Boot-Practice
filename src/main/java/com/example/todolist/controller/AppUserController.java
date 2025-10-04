7623e8df64de6d6100122a76611710b8a9a94e32
cksnd1223 <rlacksdn1223@naver.com>
2025-10-04 11:42:21 +0900

    유저 비밀번호 변경 기능 추가

--- src/main/java/com/example/todolist/controller/AppUserController.java
+++ src/main/java/com/example/todolist/controller/AppUserController.java
@@ -2,10 +2,13 @@ package com.example.todolist.controller;
 
 import com.example.todolist.dto.AccountCredentialsRecord;
 import com.example.todolist.dto.AppUserRecord;
+import com.example.todolist.dto.ChangePasswordRecord;
 import com.example.todolist.service.AppUserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,13 +19,20 @@ import java.util.List;
 public class AppUserController {
     private final AppUserService appUserService;
 
-    @GetMapping("/all")
+    @GetMapping("users/all")
+    @PreAuthorize("hasRole('ADMIN')") // ADMIN 역할을 가진 사용자만 접근 가능
     public List<AppUserRecord> getAllUsers() {
         return appUserService.getAllUser();
     }
 
-    @PostMapping("/save")
+    @PostMapping("users/save")
     public ResponseEntity<AccountCredentialsRecord> saveUser(@RequestBody AccountCredentialsRecord request) {
         return appUserService.saveUser(request);
+    }
+
+    @PatchMapping("users/changePassword")
+    public ResponseEntity<Void> changePassword(@RequestBody ChangePasswordRecord changePasswordRecord, @AuthenticationPrincipal UserDetails userDetails){
+        appUserService.changePassword(changePasswordRecord, userDetails);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/example/todolist/service/AppUserService.java
+++ src/main/java/com/example/todolist/service/AppUserService.java
@@ -2,18 +2,22 @@ package com.example.todolist.service;
 
 import com.example.todolist.dto.AccountCredentialsRecord;
 import com.example.todolist.dto.AppUserRecord;
+import com.example.todolist.dto.ChangePasswordRecord;
 import com.example.todolist.entity.AppUser;
+import com.example.todolist.exception.AccessDeniedException;
+import com.example.todolist.exception.ResourceNotFoundException;
 import com.example.todolist.exception.UsernameAlreadyExistsException;
 import com.example.todolist.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -39,5 +43,18 @@ public class AppUserService {
         }
         // searchUser가 비어있지 않다면 회원가입 불가능하니 예외처리
         else throw new UsernameAlreadyExistsException("이미 존재하는 사용자입니다: " + request.username());
+    }
+
+    @Transactional
+    public void changePassword(ChangePasswordRecord changePasswordRecord , UserDetails userDetails) {
+        // 로그인한 유저로 DB에서 정보 찾아 저장
+        AppUser loginUser = appUserRepository.findByUsername(userDetails.getUsername())
+                .orElseThrow(() -> new ResourceNotFoundException("사용자를 찾을 수 없습니다: " + userDetails.getUsername()));
+        // 입력한 현재 패스워드랑 저장된 loginUser의 패스워드가 다르면 예외 발생
+        if(!passwordEncoder.matches(changePasswordRecord.currentPassword(), loginUser.getPassword())){
+            throw new AccessDeniedException("현재 비밀번호가 일치하지 않습니다.");
+        }
+        // 일치하다면 새로운 패스워드 암호화후 변경
+        loginUser.setPassword(passwordEncoder.encode(changePasswordRecord.newPassword()));
     }
 }
